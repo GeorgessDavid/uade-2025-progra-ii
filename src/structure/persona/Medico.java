504db3ae164ce7d6860e08fe5b52e7a31429c410
santiGalarza <s4n7ini7o@gmail.com>
2025-04-24 15:36:43 -0300

    update medico & paciente

--- src/structure/persona/Medico.java
+++ src/structure/persona/Medico.java
@@ -7,5 +7,13 @@ public class Medico extends Persona{
     public Medico(String nombre, String apellido, String fechaNacimiento, int id) {
         super(nombre, apellido, fechaNacimiento, id);
     }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
 
--- src/structure/persona/Paciente.java
+++ src/structure/persona/Paciente.java
@@ -1,7 +1,18 @@
 package structure.persona;
 
 public class Paciente extends Persona {
+
+    Medico medicoAsignado;
+
     public Paciente(String nombre, String apellido, String fechaNacimiento, int id) {
         super(nombre, apellido, fechaNacimiento, id);
+    }
+
+    public void setMedicoAsignado(Medico medico) {
+        medicoAsignado = medico;
+    }
+
+    public Medico getMedicoAsignado() {
+        return medicoAsignado;
     }
 }
