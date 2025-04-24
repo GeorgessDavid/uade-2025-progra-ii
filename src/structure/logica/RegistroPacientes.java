9f3c626d3e43f8a8d3ed0e2f2fae4c8d594c92fb
santiGalarza <s4n7ini7o@gmail.com>
2025-04-23 21:44:32 -0300

    add folder "logica" & main

---
+++ src/structure/logica/RegistroPacientes.java
@@ -0,0 +1,11 @@
+package structure.logica;
+
+import structure.persona.Persona;
+import structure.implementacion.DynamicPriorityQueueADT;
+
+public class RegistroPacientes {
+
+    DynamicPriorityQueueADT colaPacientes = new DynamicPriorityQueueADT();
+
+
+}
