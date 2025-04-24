ef5f7a920a545c619e01dcc265007118fb1b233a
santiGalarza <s4n7ini7o@gmail.com>
2025-04-24 00:20:30 -0300

    add registro pacientes

--- src/structure/logica/RegistroPacientes.java
+++ src/structure/logica/RegistroPacientes.java
@@ -1,11 +1,61 @@
 package structure.logica;
 
-import structure.persona.Persona;
 import structure.implementacion.DynamicPriorityQueueADT;
+
+import structure.persona.Paciente;
+import structure.persona.Medico;
+import structure.implementacion.DynamicLinkedList;
 
 public class RegistroPacientes {
 
-    DynamicPriorityQueueADT colaPacientes = new DynamicPriorityQueueADT();
+    private final String PRIORIDAD_ALTA = "ALTA";
+    private final String PRIORIDAD_MEDIA = "MEDIA";
+    private final String PRIORIDAD_BAJA = "BAJA";
+
+    private DynamicPriorityQueueADT cola;
+    private DynamicLinkedList pacientes;
+    private DynamicLinkedList medicos;
+
+    private int siguienteIdPaciente;
+    private int siguienteIdMedico;
+
+    public RegistroPacientes() {
+        cola = new DynamicPriorityQueueADT();
+        pacientes = new DynamicLinkedList();
+        medicos = new DynamicLinkedList();
+        siguienteIdPaciente = 0;
+        siguienteIdMedico = 0;
+    }
 
 
+    public void registrarPaciente(String nombre, String apellido, String fechaNacimiento, String prioridad) {
+        Paciente paciente = new Paciente(nombre, apellido, fechaNacimiento, siguienteIdPaciente);
+        pacientes.add(paciente.getId());
+
+        int prioridadNumerica = asignarPrioridad(prioridad);
+        if(prioridadNumerica != -1){
+            cola.add(siguienteIdPaciente, prioridadNumerica);
+            siguienteIdPaciente++;
+        } else{
+            System.out.println("La prioridad ingresada no es valida. Ejemplos validos: ALTA, MEDIA, BAJA");
+        }
+    }
+
+    public int asignarPrioridad(String prioridad){
+        int numeroPrioridad = -1;
+        switch(prioridad.toUpperCase()){
+            case PRIORIDAD_ALTA:
+                numeroPrioridad = 1;
+                break;
+            case PRIORIDAD_MEDIA:
+                numeroPrioridad = 2;
+                break;
+            case PRIORIDAD_BAJA:
+                numeroPrioridad = 3;
+                break;
+            default: return numeroPrioridad;
+        }
+
+        return numeroPrioridad;
+    }
 }
